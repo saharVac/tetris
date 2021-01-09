8a72187c0e481d0ce1a9a09ee7e40f01238e2559
saharVac <vcsahar@gmail.com>
2021-01-09 15:57:11 -0500

    Added shapes

--- app.js
+++ app.js
@@ -5,6 +5,42 @@ document.addEventListener('DOMContentLoaded', () => {
     const socreDisplay = document.querySelector('%score')
     const width = 10
 
+    // Shapes
+    const lTetrimino = [
+        [1, width+1, (width*2)+1, 2],
+        [width, width+1, width+2, (width*2)+2],
+        [1, width+1, (width*2)+1, width*2],
+        [width, width*2, (width*2)+1, (width*2)+2]
+    ]
+
+    const ZTetrimino = [
+        [0, width, width+1, (width*2)+1],
+        [width + 1, width+2, width*2, (width*2)+1],
+        [0, width, width+1, (width*2)+1],
+        [width + 1, width+2, width*2, (width*2)+1]
+    ]
     
+    const tTetrimino = [
+        [1, width, width+1, width+2],
+        [1, width+1, width+2, (width*2)+1],
+        [width, width+1, width+2, (width*2)+1],
+        [1, width, width+1, (width*2)+1]
+    ]
+    
+    const oTetrimino = [
+        [0, 1, width, width+1],
+        [0, 1, width, width+1],
+        [0, 1, width, width+1],
+        [0, 1, width, width+1]
+    ]
+    
+    const iTetrimino = [
+        [1, width+1, (width*2)+1, (width*3)+1],
+        [width, width+1, width+2, width+3],
+        [1, width+1, (width*2)+1, (width*3)+1],
+        [width, width+1, width+2, width+3]
+    ]
+
+    const theTetriminoes = [lTetrimino, ZTetrimino, tTetrimino, oTetrimino, iTetrimino]
 
 })
