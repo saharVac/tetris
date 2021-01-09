d938db9d32e22ad4a26d8697399b3399ef651eb2
saharVac <vcsahar@gmail.com>
2021-01-09 15:44:46 -0500

    Declared dependencies

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 document.addEventListener('DOMContentLoaded', () => {
+    
+    const grid = document.querySelector('.grid')
+    let squares = Array.from(document.querySelectorAll('.grid div'))
+    const socreDisplay = document.querySelector('%score')
+    const width = 10
 
-
-
+    
 
 })
